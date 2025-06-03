dc9d4722c0b0dc1e2b58003b69bf3b46384bbf2b
Tero Laxström <tlax@neonpeons.com>
2025-06-03 20:52:26 +0300

    Components around

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,36 @@
 use bevy::{app::ScheduleRunnerPlugin, prelude::*};
 use std::time::Duration;
 
+mod blob;
+use blob::Blob;
+mod position;
+use position::Position;
+
 fn main() {
     App::new()
         .add_plugins(
             MinimalPlugins.set(ScheduleRunnerPlugin::run_loop(Duration::from_secs_f64(1.0))),
         )
-        .add_systems(Update, update)
+        .add_systems(Startup, setup)
+        .add_systems(Update, (move_blobs, show_blobs).chain())
         .run();
 }
 
-fn update() {
-    println!(".");
+fn setup(mut commands: Commands) {
+    for x in 1..10 {
+        commands.spawn((blob::Blob(x.to_string()), position::Position(0.0, 0.0)));
+    }
+}
+
+fn move_blobs(query: Query<&mut Position, With<Blob>>) {
+    for mut p in query {
+        p.0 += 1.;
+        p.1 += 1.;
+    }
+}
+
+fn show_blobs(query: Query<(&Blob, &Position)>) {
+    for (Blob(blob), Position(x, y)) in query {
+        println!("{} {} {}", blob, x, y);
+    }
 }
---
+++ src/position.rs
@@ -0,0 +1,4 @@
+use bevy::prelude::*;
+
+#[derive(Component, Debug)]
+pub struct Position(pub f32, pub f32);
